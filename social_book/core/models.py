c4d91e97e374f1fe9bc38670b6f4f5dda2c76747
LimbaniDarshan <dblimbani145@gmail.com>
2024-07-15 23:56:59 +0530

    first commit

---
+++ social_book/core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User=get_user_model()
+
+class Profile(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    id_user=models.IntegerField()
+    bio=models.TextField(blank=True)
+    profileimg=models.ImageField(upload_to='profile_image', default='blank-profile-picture-973460_1280.png')
+    location=models.CharField(max_length=100,blank=True)
+    
+    class Meta:
+        db_table = 'Profile'
+    
+    def __str__(self):
+        return self.user.username
